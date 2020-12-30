8619a77b156d90f4383ad78696b136d5b82852f5
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-30 15:04:07 -0600

    add context

--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        "authors": Author.objects.all(),
+        "books": Book.objects.all()
+    }
+    return render(request, 'index.html', context)
